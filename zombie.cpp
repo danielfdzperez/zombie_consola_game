e18afec508daddb86654316cd613ac33615720e3
danielfdzperez <daniel.fdz.perez@gmail.com>
2014-04-02 19:31:53 +0200

    testing

--- zombie.cpp
+++ zombie.cpp
@@ -5,14 +5,28 @@
 #include <string.h>
 
 #define EXIT 27
-#define NENEMIES 100
+#define NENEMIES 5
 struct TCharacter{
     int moviment;
     int live;
     int posx;
     int posy;
     char look;
+    bool is_moving;
 };
+
+void enemies_spawn(struct TCharacter enemy[], int *enemy_index){
+
+    //if(rand() % 100 == 37)
+    if(*enemy_index < NENEMIES)
+	if(!enemy[*enemy_index].is_moving){
+	    enemy[*enemy_index].look = 'M';
+	    enemy[*enemy_index].posy = enemy[*enemy_index].posx = 20+ rand() % 5;
+	    enemy[*enemy_index].is_moving = true;
+	    (*enemy_index) ++;
+	}
+
+}
 
 /*Move player*/
 void player_moviment(struct TCharacter *player){
@@ -22,7 +36,7 @@ void player_moviment(struct TCharacter *player){
 	case KEY_RIGHT:
 	    if((*player).posx + 1 != COLS)
 		(*player).posx += 1;
-	    
+
 	    break;
 
 	case KEY_LEFT:
@@ -42,27 +56,50 @@ void player_moviment(struct TCharacter *player){
     }
 }
 
+void enemies_moviment(struct TCharacter enemy[NENEMIES], int *enemy_index){
 
-void print_game(struct TCharacter player, struct TCharacter enemy[NENEMIES]){
+    for(int move=0; move<NENEMIES; move++){
+	if(enemy[move].posx + 1 == COLS){
+	    enemy[move].is_moving = false;
+	    (*enemy_index) --;
+	}
+	enemy[move].posx += 1;
+    }
+
+
+}
+
+void print_game(struct TCharacter player, struct TCharacter enemy[NENEMIES], int enemy_index){
     clear();
     mvprintw(player.posy, player.posx, "%c", player.look);
-    for(int print_enemies = 0; print_enemies < NENEMIES; )
-    mvprintw(enemy[0].posy, enemy[0].posx, "%c", enemy[0].look);
+    printw("%i\n", enemy_index);
+    for(int print_enemies = 0; print_enemies < NENEMIES; print_enemies ++)
+	if(enemy[print_enemies].is_moving)
+	    mvprintw(enemy[print_enemies].posy, enemy[print_enemies].posx, "%c", enemy[print_enemies].look);
     refresh();
 
 }
 void loop_game(){
 
+    int enemy_index;
     struct TCharacter player;
     struct TCharacter enemy[NENEMIES];
+
     player.look = 'P';
     player.posy = player.posx = 10;
-    enemy[0].look = 'M';
-    enemy[0].posy = enemy[0].posx = 20;
+
+    for(int i = 0; i < NENEMIES; i++)
+	enemy[i].is_moving = false;
+
+    enemy_index = 0;
+
     do{
-	print_game(player, enemy);
+	timeout ( 200 );
+	print_game(player, enemy, enemy_index);
+	enemies_spawn(enemy, &enemy_index);
 	player.moviment = getch();
 	player_moviment(&player);
+	enemies_moviment(enemy, &enemy_index);
     }while(player.moviment != EXIT);
 
 }
@@ -74,6 +111,7 @@ int main(int argc, char *argv[]){
     noecho();
     start_color();
     curs_set(0);
+    srand(time(NULL));
 
     loop_game();
 
