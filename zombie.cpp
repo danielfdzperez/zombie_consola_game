f42bbd35bad00be625cf70f642596ccaf20be12c
Daniel Fernandez Perez <daniel.fdz.perez@gmail.com>
2014-04-08 11:27:13 +0200

    testing enemy moviment

--- zombie.cpp
+++ zombie.cpp
@@ -8,13 +8,13 @@
 #define NENEMIES 5
 enum TSide {left, top, right, bottom};
 struct TCharacter{
-    int moviment;
-    int live;
-    int posx;
-    int posy;
-    char look;
-    bool is_moving;
-    int move;
+    int moviment; //the direction
+    int live; //the live 
+    int posx; //x position = column
+    int posy; //y position = row
+    char look; //character ex: 'M'
+    bool is_moving; //especify the enemy is in moviment
+    int move; //?
 };
 
 void enemies_spawn(struct TCharacter enemy[], int *enemy_index){
@@ -49,8 +49,8 @@ void enemies_spawn(struct TCharacter enemy[], int *enemy_index){
     (*enemy_index) ++;
 }
 
-/*Move player*/
-void player_moviment(struct TCharacter *player){
+/*Actions of  player*/
+void player_actions(struct TCharacter *player){
 
     switch((*player).moviment){
 
@@ -77,23 +77,30 @@ void player_moviment(struct TCharacter *player){
     }
 }
 
-void enemies_moviment(struct TCharacter enemy[NENEMIES], int *enemy_index){
+void enemies_moviment(struct TCharacter enemy[NENEMIES], int *enemy_index, struct TCharacter player){
 
-    for(int move=0; move<NENEMIES; move++){
-	if(enemy[move].posx + 1 == COLS){
-	    enemy[move].is_moving = false;
-	}
-	enemy[move].posx += 1;
-    }
+ //   for(int move=0; move<NENEMIES; move++)
+//	if(abs(player.posx - enemy[move].posx) < abs(player.posy - enemy[move].posy))
+//	    enemy[move].posx --; 
+//	else
+//	    enemy[move].posy ++;
+    /*
+       for(int move=0; move<NENEMIES; move++){
+       if(enemy[move].posx + 1 == COLS){
+       enemy[move].is_moving = false;
+       }
+       enemy[move].posx += 1;
+       }*/
 }
 
 void print_game(struct TCharacter player, struct TCharacter enemy[NENEMIES], int enemy_index){
     clear();
     mvprintw(player.posy, player.posx, "%c", player.look);
-    printw("%i\n", enemy_index);
+    //printw("%i\n", enemy_index);
     for(int print_enemies = 0; print_enemies < NENEMIES; print_enemies ++)
 	if(enemy[print_enemies].is_moving)
 	    mvprintw(enemy[print_enemies].posy, enemy[print_enemies].posx, "%c", enemy[print_enemies].look);
+    printw("row = %i col = %i", abs(enemy[0].posy-player.posy), abs(enemy[0].posx-player.posx));
     refresh();
 
 }
@@ -115,13 +122,13 @@ void loop_game(){
     enemy_index = 0;
 
     do{
-	timeout ( 300 );
+	//timeout ( 300 );
 	print_game(player, enemy, enemy_index);
 	if(enemy_index < NENEMIES )
 	    enemies_spawn(enemy, &enemy_index);
 	player.moviment = getch();
-	player_moviment(&player);
-	enemies_moviment(enemy, &enemy_index);
+	player_actions(&player);
+	enemies_moviment(enemy, &enemy_index, player);
     }while(player.moviment != EXIT);
 
 }
