274de1c7ce21edd315b9799ad6307acbfe167293
danielfdzperez <daniel.fdz.perez@gmail.com>
2014-04-16 19:29:47 +0200

    change all game

--- zombie.cpp
+++ zombie.cpp
@@ -10,6 +10,14 @@
 #define VEL 100
 enum TSide {left, top, right, bottom};
 
+struct TScreen{
+    char **board;
+    int size_x;
+    int size_y;
+    int max_x;
+    int max_y;
+};
+
 struct TPosition{
     int x;
     int y;
@@ -31,6 +39,70 @@ struct TCharacter{
     int move; //?
     int attack;
 };
+
+/*Testing*/
+void pintar(char **tablero, int tamano_x, int tamano_y){
+
+    clear();
+    for(int y=0; y<tamano_y; y++){
+	for(int x=0; x<tamano_x; x++)
+	    printw("%c",tablero[y][x]);
+	printw("\n");
+    }
+    refresh();
+}
+
+void fill_in(struct TScreen board_game){
+    for(int i=0; i<board_game.size_y; i++)
+	memset(board_game.board[i], ' ', board_game.size_x * sizeof(char));
+    for(int y=0; y<board_game.size_y; y++)
+	for(int x=0; x<board_game.size_x; x++)
+	    if(y == 0 || y == board_game.size_y - 1 || x == 0 || x == board_game.size_x - 1)
+		board_game.board[y][x] = 'H';
+}
+
+char **resize(struct TScreen *board_game){
+    int diferencia;
+    if(board_game->size_x > COLS-1)
+	board_game->size_x = COLS - 1;
+
+    if(board_game->size_y > LINES)
+	board_game->size_y = LINES;
+
+    if(board_game->size_x < COLS-1)
+	if (COLS - 1 <= board_game->max_x)
+	    board_game->size_x = COLS - 1;
+	else{
+	    diferencia = (COLS-1) - board_game->size_x;
+	    board_game->max_x = board_game->size_x = COLS - 1; 
+	    for (int i= 0; i<board_game->size_y; i++)
+		board_game->board[i] = (char *) malloc(board_game->size_x * sizeof(char));
+	}
+
+    if(board_game->size_y < LINES)
+	if(LINES <= board_game->max_y)
+	    board_game->size_y = LINES;
+	else{
+	    diferencia = LINES - board_game->size_y;
+	    board_game->max_y = board_game->size_y = LINES;
+	    board_game->board =(char**) realloc(board_game->board, board_game->size_y * sizeof(char*));
+	    for(int i = 0; i<diferencia; i++){
+		board_game->board[board_game->size_y-(i+1)] = (char*) malloc(board_game->size_x * sizeof(char));
+	    }
+	}
+    fill_in(*board_game);
+    return board_game->board;
+}
+
+char ** initialize(struct TScreen board_game){
+    board_game.board = (char **) malloc(board_game.size_y * sizeof (char *));
+    for (int i= 0; i<board_game.size_y; i++)
+	board_game.board[i] = (char *) malloc(board_game.size_x * sizeof(char));
+
+    fill_in(board_game);
+    return board_game.board;
+
+}
 
 void enemies_spawn(struct TCharacter enemy[], int *enemy_index){
     bool be_born=true;
@@ -205,7 +277,7 @@ void enemy_atack(struct TCharacter *player, struct TCharacter enemy[],int enemy_
 
 }
 /*Actions of  player*/
-void player_actions(struct TCharacter *player, struct TCharacter enemy[]){
+void player_actions(struct TCharacter *player, struct TCharacter enemy[], char **board){
 
     bool enemy_position = false;
     switch(player->moviment){
@@ -253,6 +325,8 @@ void player_actions(struct TCharacter *player, struct TCharacter enemy[]){
 	case SPACE:
 	    break;
     }
+
+    board[player->posy][player->posx] = 'O';
 }
 
 void print_game(struct TCharacter player, struct TCharacter enemy[NENEMIES], int enemy_index){
@@ -266,13 +340,13 @@ void print_game(struct TCharacter player, struct TCharacter enemy[NENEMIES], int
     mvprintw(LINES/2, COLS/2, "%i", enemy_index);
     refresh();
 }
-
 void loop_game(){
-
+    erase();
     int enemy_index;
     struct TCharacter player;
     struct TCharacter enemy[NENEMIES];
     struct TWeapon gun[5];
+    struct TScreen board_game;
 
     player.look = 'P';
     player.posy = player.posx = 10;
@@ -290,20 +364,33 @@ void loop_game(){
 	enemy[i].move = 0;
     }
 
+    board_game.max_x = board_game.size_x = COLS - 1;
+    board_game.max_y = board_game.size_y = LINES;
+
+    board_game.board = initialize(board_game);
 
     enemy_index = 0;
 
     do{
-	timeout ( 5 );
-	print_game(player, enemy, enemy_index);
+		timeout ( 5 );
+	if(board_game.size_x < COLS-1 || board_game.size_x > COLS-1 || board_game.size_y < LINES || board_game.size_y > LINES)
+	    board_game.board = resize(&board_game);
+	//print_game(player, enemy, enemy_index);
 	if(enemy_index < NENEMIES )
 	    enemies_spawn(enemy, &enemy_index);
 	player.moviment = getch();
-	player_actions(&player, enemy);
+	player_actions(&player, enemy, board_game.board);
 	enemies_moviment(enemy, player, enemy_index);
 	enemy_atack(&player, enemy, enemy_index);
+
+	//testing
+	pintar( board_game.board, board_game.size_x, board_game.size_y);
+
     }while(player.moviment != EXIT);
 
+    for(int i = 0; i<board_game.size_y; i++)
+	free(board_game.board[i]);
+    free(board_game.board);
 }
 
 int main(int argc, char *argv[]){
