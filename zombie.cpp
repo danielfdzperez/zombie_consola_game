d2c05f9d38224f5934079d55bc42092652b0dfde
danielfdzperez <daniel.fdz.perez@gmail.com>
2014-04-18 13:22:58 +0200

    create weapon struct, can change guns, print gun, add direccion

--- zombie.cpp
+++ zombie.cpp
@@ -5,10 +5,14 @@
 #include <string.h>
 
 #define EXIT 27
-#define NENEMIES 10000
+#define NENEMIES 1000
 #define SPACE 32
 #define VEL 100
+#define ENEMY 'x'
+#define WEAPONS 2
 enum TSide {left, top, right, bottom};
+enum TOrientation {north, east, south, west};
+enum TGuns {gun, rifle};
 
 struct TScreen{
     char **board;
@@ -16,6 +20,7 @@ struct TScreen{
     int size_y;
     int max_x;
     int max_y;
+    int border;
 };
 
 struct TPosition{
@@ -23,10 +28,18 @@ struct TPosition{
     int y;
 };
 
-struct TWeapon{
+struct TShot{
     struct TPosition pos;
     bool is_moving;
-    int live;
+};
+
+struct TWeapon{
+    int max_amo;//max amo
+    int max_shots;//number of shots that can be fired
+    int bullets_per_shot;
+    int live;//live rest the gun
+    int index;
+    struct TShot *bullets;//number of bullets, the gun can shot
 };
 
 struct TCharacter{
@@ -36,45 +49,40 @@ struct TCharacter{
     int posy; //y position = row
     char look; //character ex: 'M'
     bool is_moving; //especify the enemy is in moviment
-    int move; //?
-    int attack;
+    int move; //the time needs a zombie to move
+    int attack;//the time needs a zombie to atack
+    int orientation;
+    int selected_weapon;
 };
-
-/*Testing*/
-void pintar(char **tablero, int tamano_x, int tamano_y){
-
-    clear();
-    for(int y=0; y<tamano_y; y++){
-	for(int x=0; x<tamano_x; x++)
-	    printw("%c",tablero[y][x]);
-	printw("\n");
-    }
-    refresh();
-}
 
 void fill_in(struct TScreen board_game){
     for(int i=0; i<board_game.size_y; i++)
 	memset(board_game.board[i], ' ', board_game.size_x * sizeof(char));
     for(int y=0; y<board_game.size_y; y++)
 	for(int x=0; x<board_game.size_x; x++)
-	    if(y == 0 || y == board_game.size_y - 1 || x == 0 || x == board_game.size_x - 1)
+	    if(y == 0 || y == board_game.size_y - 1 || x == 0 || x == board_game.border)
 		board_game.board[y][x] = 'H';
 }
 
 char **resize(struct TScreen *board_game){
     int diferencia;
-    if(board_game->size_x > COLS-1)
+    if(board_game->size_x > COLS-1){
 	board_game->size_x = COLS - 1;
+	board_game->border = board_game->size_x - 10;
+    }
 
     if(board_game->size_y > LINES)
 	board_game->size_y = LINES;
 
     if(board_game->size_x < COLS-1)
-	if (COLS - 1 <= board_game->max_x)
+	if (COLS - 1 <= board_game->max_x){
 	    board_game->size_x = COLS - 1;
+	    board_game->border = board_game->size_x - 10;
+	}
 	else{
 	    diferencia = (COLS-1) - board_game->size_x;
 	    board_game->max_x = board_game->size_x = COLS - 1; 
+	    board_game->border = board_game->size_x - 10;
 	    for (int i= 0; i<board_game->size_y; i++)
 		board_game->board[i] = (char *) malloc(board_game->size_x * sizeof(char));
 	}
@@ -104,65 +112,50 @@ char ** initialize(struct TScreen board_game){
 
 }
 
-void enemies_spawn(struct TCharacter enemy[], int *enemy_index){
-    bool be_born=true;
+/*to do: change board to board_game.board*/
+void enemies_spawn(struct TCharacter enemy[], int *enemy_index, char **board, struct TScreen board_game){
+    bool be_born=false;
     int pos;
     if(rand() % 56 == 3){
 	switch(rand() % 4){
 	    case left:
 		if(!enemy[*enemy_index].is_moving){
-		    pos = rand() % LINES;
-		    for(int i = 0; i < *enemy_index; i++)
-			if(enemy[i].posy == pos && enemy[i].posx == 0){
-			    be_born = false;
-			    break;
-			}
-			else{
-			    enemy[*enemy_index].posy = pos;
-			    enemy[*enemy_index].posx = 0;
-			}
+		    pos = rand() % board_game.size_y;
+		    if(board[pos][1] != ENEMY && board[pos][1] != 'H'){
+			enemy[*enemy_index].posy = pos;
+			enemy[*enemy_index].posx = 1;
+			be_born = true;
+		    }
 		}
 		break;
 	    case top:
 		if(!enemy[*enemy_index].is_moving){
-		    pos = rand() % COLS;
-		    for(int i = 0; i < *enemy_index; i++)
-			if(enemy[i].posy == 0 && enemy[i].posx == pos){
-			    be_born = false;
-			    break;
-			}
-			else{
-			    enemy[*enemy_index].posy = 0;
-			    enemy[*enemy_index].posx = pos;
-			}
+		    pos = rand() % board_game.border;
+		    if(board[1][pos] != ENEMY && board[1][pos] != 'H'){
+			enemy[*enemy_index].posy = 1;
+			enemy[*enemy_index].posx = pos;
+			be_born = true;
+		    }
 		}
 		break;
 	    case right:
 		if(!enemy[*enemy_index].is_moving){
-		    pos = rand() % LINES;
-		    for(int i = 0; i < *enemy_index; i++)
-			if(enemy[i].posy == pos && enemy[i].posx == COLS-1){
-			    be_born = false;
-			    break;
-			}
-			else{
-			    enemy[*enemy_index].posy = pos;
-			    enemy[*enemy_index].posx = COLS-1;
-			}
+		    pos = rand() % board_game.size_y;
+		    if(board[pos][board_game.border-2] != ENEMY && board[pos][board_game.border-2] != 'H'){
+			enemy[*enemy_index].posy = pos;
+			enemy[*enemy_index].posx = board_game.border-2;
+			be_born = true;
+		    }
 		}
 		break;
 	    case bottom:
 		if(!enemy[*enemy_index].is_moving){
-		    pos = rand() % COLS;
-		    for(int i = 0; i < *enemy_index; i++)
-			if(enemy[i].posy == LINES-1 && enemy[i].posx == pos){
-			    be_born = false;
-			    break;
-			}
-			else{
-			    enemy[*enemy_index].posy = LINES-1;
-			    enemy[*enemy_index].posx = pos;
-			}
+		    pos = rand() % board_game.border;
+		    if(board[board_game.size_y-2][pos] != ENEMY && board[board_game.size_y-2][pos] != 'H'){
+			enemy[*enemy_index].posy = board_game.size_y-2;
+			enemy[*enemy_index].posx = pos;
+			be_born = true;
+		    }
 		}
 		break;
 	}
@@ -174,7 +167,11 @@ void enemies_spawn(struct TCharacter enemy[], int *enemy_index){
     }
 }
 
-void enemies_moviment(struct TCharacter enemy[NENEMIES], struct TCharacter player, int enemy_index){
+void enemy_atack(struct TCharacter *player, struct TCharacter enemy[],int enemy_index){
+
+}
+/*Mejorar IA, cuando las columnas o las filas son menores que el otro va mal*/
+void enemies_moviment(struct TCharacter enemy[NENEMIES], struct TCharacter player, int enemy_index, char **board){
 
     bool dont_muve = false;
     for(int move=0; move<enemy_index; move++){//pasa por todos los enemigos hasta el momento
@@ -185,212 +182,253 @@ void enemies_moviment(struct TCharacter enemy[NENEMIES], struct TCharacter playe
 
 		    if(player.posy > enemy[move].posy){
 
-			if(player.posx == enemy[move].posx && player.posy == enemy[move].posy + 1)
+			if(board[enemy[move].posy + 2][enemy[move].posx] == player.look || 
+				board[enemy[move].posy + 1][enemy[move].posx] == player.look)
 			    dont_muve = true;
-			for(int i=0; i<enemy_index && dont_muve == false; i++)
-			    if(enemy[i].posx == enemy[move].posx && enemy[i].posy == enemy[move].posy + 1)
-				dont_muve = true;
+			if(board[enemy[move].posy + 1][enemy[move].posx] == ENEMY)
+			    dont_muve = true;
 
-			if(!dont_muve)
+			if(!dont_muve){
+			    board[enemy[move].posy][enemy[move].posx] = ' ';
 			    enemy[move].posy++;
+			}
 		    }
 
 		    else{
-			if(player.posx == enemy[move].posx && player.posy == enemy[move].posy - 1)
+			if(board[enemy[move].posy - 2][enemy[move].posx] == player.look || 
+				board[enemy[move].posy - 1][enemy[move].posx] == player.look)
 			    dont_muve = true;
-			for(int i=0; i<enemy_index && dont_muve == false; i++)
-			    if(enemy[i].posx == enemy[move].posx && enemy[i].posy == enemy[move].posy - 1)
-				dont_muve = true;
+			if(board[enemy[move].posy - 1][enemy[move].posx] == ENEMY)
+			    dont_muve = true;
 
-			if(!dont_muve)
+			if(!dont_muve){
+			    board[enemy[move].posy][enemy[move].posx] = ' ';
 			    enemy[move].posy--;
+			}
 		    }
 		}
 		else
 		    if(player.posy == enemy[move].posy){
 			if(player.posx > enemy[move].posx){
-			    if(player.posx == enemy[move].posx + 1 && player.posy == enemy[move].posy)
+			    if(board[enemy[move].posy][enemy[move].posx + 2] == player.look ||
+				    board[enemy[move].posy][enemy[move].posx + 1] == player.look )
 				dont_muve = true;
-			    for(int i=0; i<enemy_index && dont_muve == false; i++)
-				if(enemy[i].posx == enemy[move].posx + 1 && enemy[i].posy == enemy[move].posy)
-				    dont_muve = true;
+			    if(board[enemy[move].posy][enemy[move].posx + 1] == ENEMY)
+				dont_muve = true;
 
-			    if(!dont_muve)
+			    if(!dont_muve){
+				board[enemy[move].posy][enemy[move].posx] = ' ';
 				enemy[move].posx++;
+			    }
 			}
 			else{
-			    if(player.posx == enemy[move].posx - 1 && player.posy == enemy[move].posy)
+			    if(board[enemy[move].posy][enemy[move].posx - 2] == player.look ||
+				    board[enemy[move].posy][enemy[move].posx - 1] == player.look)
 				dont_muve = true;
-			    for(int i=0; i<enemy_index && dont_muve == false; i++)
-				if(enemy[i].posx == enemy[move].posx - 1 && enemy[i].posy == enemy[move].posy)
-				    dont_muve = true;
+			    if(board[enemy[move].posy][enemy[move].posx - 1] == ENEMY)
+				dont_muve = true;
 
-			    if(!dont_muve)
+			    if(!dont_muve){
+				board[enemy[move].posy][enemy[move].posx] = ' ';
 				enemy[move].posx--;
+			    }
 			}
 
 		    }
 		    else
 			if(abs(player.posx - enemy[move].posx) < abs(player.posy - enemy[move].posy))
 			    if(player.posx < enemy[move].posx){
-				for(int i=0; i<enemy_index && dont_muve == false; i++)
-				    if(enemy[i].posx == enemy[move].posx - 1 && enemy[i].posy == enemy[move].posy)
-					dont_muve = true;
 
-				if(!dont_muve)
+				if(board[enemy[move].posy][enemy[move].posx - 1] != ENEMY){
+				    board[enemy[move].posy][enemy[move].posx] = ' ';
 				    enemy[move].posx --;
+				}
 			    }
 			    else{
-				for(int i=0; i<enemy_index && dont_muve == false; i++)
-				    if(enemy[i].posx == enemy[move].posx + 1 && enemy[i].posy == enemy[move].posy)
-					dont_muve = true;
-
-				if(!dont_muve)
+				if(board[enemy[move].posy][enemy[move].posx + 1] != ENEMY){
+				    board[enemy[move].posy][enemy[move].posx] = ' ';
 				    enemy[move].posx ++;
+				}
 			    }
 
 			else
 			    if(player.posy < enemy[move].posy){
-				for(int i=0; i<enemy_index && dont_muve == false; i++)
-				    if(enemy[i].posx == enemy[move].posx && enemy[i].posy == enemy[move].posy - 1)
-					dont_muve = true;
-
-				if(!dont_muve)
+				if(board[enemy[move].posy - 1][enemy[move].posx] != ENEMY){
+				    board[enemy[move].posy][enemy[move].posx] = ' ';
 				    enemy[move].posy --;
+				}
 			    }
 			    else{
-				for(int i=0; i<enemy_index && dont_muve == false; i++)
-				    if(enemy[i].posx == enemy[move].posx && enemy[i].posy == enemy[move].posy + 1)
-					dont_muve = true;
-
-				if(!dont_muve)
+				if(board[enemy[move].posy + 1][enemy[move].posx] != ENEMY){
+				    board[enemy[move].posy][enemy[move].posx] = ' ';
 				    enemy[move].posy ++;
+				}
 			    }
 		enemy[move].move = 0;
 	    }
 	    else
 		enemy[move].move ++;
+	board[enemy[move].posy][enemy[move].posx] = enemy[move].look;
     }
 }
 
-void enemy_atack(struct TCharacter *player, struct TCharacter enemy[],int enemy_index){
-
-}
 /*Actions of  player*/
 void player_actions(struct TCharacter *player, struct TCharacter enemy[], char **board){
 
-    bool enemy_position = false;
+    switch(player->orientation){
+	case east:
+	    board[player->posy][player->posx+1] = ' ';
+	    break;
+	case west:
+	    board[player->posy][player->posx-1] = ' ';
+	    break;
+	case north:
+	    board[player->posy-1][player->posx] = ' ';
+	    break;
+	case south:
+	    board[player->posy+1][player->posx] = ' ';
+	    break;
+    }
+
     switch(player->moviment){
 
 	case KEY_RIGHT:
-	    if(player->posx + 1 != COLS-1){
-		for(int i=0; i<NENEMIES && enemy_position == false; i++)
-		    if(player->posx + 1 == enemy[i].posx && player->posy == enemy[i].posy)
-			enemy_position = true;
-		if(!enemy_position)
-		    player->posx ++;
+	    if(board[player->posy][player->posx + 2] != 'H' && board[player->posy][player->posx + 2] != ENEMY &&
+		    board[player->posy][player->posx + 1] != ENEMY){
+		board[player->posy][player->posx] = ' ';
+		player->posx ++;
+		player->orientation = east;
 	    }
 	    break;
 
 	case KEY_LEFT:
-	    if(player->posx - 1 != 0){
-		for(int i=0; i<NENEMIES && enemy_position == false; i++)
-		    if(player->posx - 1 == enemy[i].posx && player->posy == enemy[i].posy)
-			enemy_position = true;
-		if(!enemy_position)
-		    player->posx -= 1;
+	    if(board[player->posy][player->posx - 2] != 'H' && board[player->posy][player->posx - 2] != ENEMY &&
+		    board[player->posy][player->posx - 1] != ENEMY){
+		board[player->posy][player->posx] = ' ';
+		player->posx --;
+		player->orientation = west;
 	    }
 	    break;
 
 	case KEY_UP:
-	    if(player->posy - 1 != 0){
-		for(int i=0; i<NENEMIES && enemy_position == false; i++)
-		    if(player->posx  == enemy[i].posx && player->posy - 1 == enemy[i].posy)
-			enemy_position = true;
-		if(!enemy_position)
-		    player->posy -= 1;
+	    if(board[player->posy - 2][player->posx] != 'H' && board[player->posy - 2][player->posx] != ENEMY &&
+		    board[player->posy - 1][player->posx] != ENEMY){
+		board[player->posy][player->posx] = ' ';
+		player->posy --;
+		player->orientation = north;
 	    }
 	    break;
 
 	case KEY_DOWN:
-	    if(player->posy + 1 != LINES-1){
-		for(int i=0; i<NENEMIES && enemy_position == false; i++)
-		    if(player->posx == enemy[i].posx && player->posy + 1 == enemy[i].posy)
-			enemy_position = true;
-		if(!enemy_position)
-		    player->posy += 1;
+	    if(board[player->posy + 2][player->posx] != 'H' && board[player->posy + 2][player->posx] != ENEMY &&
+		    board[player->posy + 1][player->posx] != ENEMY){
+		board[player->posy][player->posx] = ' ';
+		player->posy += 1;
+		player->orientation = south;
 	    }
 	    break;
 
 	case SPACE:
 	    break;
-    }
 
-    board[player->posy][player->posx] = 'O';
+	case 'z':
+	    if (player->selected_weapon > 0)
+		player->selected_weapon --;
+	    break;
+	case 'x':
+	    if (player->selected_weapon < WEAPONS-1)
+		player->selected_weapon++;
+	    break;
+    }
+    switch(player->orientation){
+	case east:
+	    board[player->posy][player->posx+1] = '=';
+	    break;
+	case west:
+	    board[player->posy][player->posx-1] = '=';
+	    break;
+	case north:
+	    board[player->posy-1][player->posx] = '=';
+	    break;
+	case south:
+	    board[player->posy+1][player->posx] = '=';
+	    break;
+    }
+    board[player->posy][player->posx] = player->look;
 }
 
-void print_game(struct TCharacter player, struct TCharacter enemy[NENEMIES], int enemy_index){
-    //clear();
+void print_game(struct TCharacter player, struct TCharacter enemy[NENEMIES], int enemy_index, char **tablero, int tamano_x, int tamano_y){
     erase();
-    mvprintw(player.posy, player.posx, "%c", player.look);
-    for(int print_enemies = 0; print_enemies < NENEMIES; print_enemies ++)
-	if(enemy[print_enemies].is_moving)
-	    mvprintw(enemy[print_enemies].posy, enemy[print_enemies].posx, "%c", enemy[print_enemies].look);
-    printw("row = %i col = %i", abs(enemy[0].posy-player.posy), abs(enemy[0].posx-player.posx));
-    mvprintw(LINES/2, COLS/2, "%i", enemy_index);
+    for(int y=0; y<tamano_y; y++){
+	for(int x=0; x<tamano_x; x++)
+	    printw("%c",tablero[y][x]);
+	printw("\n");
+    }
+    //mvprintw(5, 10, "%i L%i C%i", enemy_index, LINES, COLS);
+    mvprintw(5, 10, "%i", player.selected_weapon);
     refresh();
 }
 void loop_game(){
-    erase();
+
     int enemy_index;
     struct TCharacter player;
     struct TCharacter enemy[NENEMIES];
-    struct TWeapon gun[5];
     struct TScreen board_game;
+    struct TWeapon guns[WEAPONS];
 
-    player.look = 'P';
+    player.look = 'O';
     player.posy = player.posx = 10;
     player.life = 100;
+    player.orientation = east;
+    player.selected_weapon = gun;
 
-#ifdef BETA    
-    for(int i = 0; i < ; i++){
-    }
-#endif
+    //initialize the gun
+    guns[gun].max_amo = 9;
+    guns[gun].max_shots = 5;
+    guns[gun].bullets_per_shot = 1;
+    guns[gun].live = 5;
+    guns[gun].index = 0;
+    guns[gun].bullets = (struct TShot*) malloc(guns[gun].max_shots * sizeof(struct TShot));
 
 
+    for(int i = 0; i < 5; i++)
+	guns[gun].bullets[i].is_moving = false;
+
+    //initialize the enemies
     for(int i = 0; i < NENEMIES; i++){
 	enemy[i].is_moving = false;
-	enemy[i].look = 'M';
+	enemy[i].look = ENEMY;
 	enemy[i].move = 0;
+	enemy[i].life = 50;
     }
 
+    //initialize the board_game
     board_game.max_x = board_game.size_x = COLS - 1;
     board_game.max_y = board_game.size_y = LINES;
+    board_game.border = board_game.size_x - 10;
 
     board_game.board = initialize(board_game);
 
     enemy_index = 0;
 
     do{
-		timeout ( 5 );
+	timeout ( 5 );
 	if(board_game.size_x < COLS-1 || board_game.size_x > COLS-1 || board_game.size_y < LINES || board_game.size_y > LINES)
 	    board_game.board = resize(&board_game);
-	//print_game(player, enemy, enemy_index);
+	print_game(player, enemy, enemy_index, board_game.board, board_game.size_x, board_game.size_y);
 	if(enemy_index < NENEMIES )
-	    enemies_spawn(enemy, &enemy_index);
+	    enemies_spawn(enemy, &enemy_index, board_game.board, board_game);
 	player.moviment = getch();
 	player_actions(&player, enemy, board_game.board);
-	enemies_moviment(enemy, player, enemy_index);
-	enemy_atack(&player, enemy, enemy_index);
-
-	//testing
-	pintar( board_game.board, board_game.size_x, board_game.size_y);
+	enemies_moviment(enemy, player, enemy_index, board_game.board);
+	//enemy_atack(&player, enemy, enemy_index);
 
     }while(player.moviment != EXIT);
 
     for(int i = 0; i<board_game.size_y; i++)
 	free(board_game.board[i]);
     free(board_game.board);
+    free(guns[gun].bullets);
 }
 
 int main(int argc, char *argv[]){
